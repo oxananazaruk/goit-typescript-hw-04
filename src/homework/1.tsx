4115fe62c750750d5436ee023c786fffc7133a5f
oxananazaruk <132213141+oxananazaruk@users.noreply.github.com>
2024-01-03 21:45:43 +0200

    Update 1.tsx

--- src/homework/1.tsx
+++ src/homework/1.tsx
@@ -1,13 +1,25 @@
 import React, { useEffect, useRef } from 'react';
 
 // Опишіть Props
+type Props = {
+  children: React.ReactNode;
+  onContentEndVisible: () => void;
+};
+
+type Options = {
+  rootMargin: string;
+  threshold: number | number[];
+  root: Element | null;
+};
+
 export function Observer({ children, onContentEndVisible }: Props) {
   // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
-  const endContentRef = useRef(null);
+  const endContentRef = useRef<HTMLDivElement>(null);
+ 
 
   useEffect(() => {
     // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
-    const options = {
+    const options: Options = {
       rootMargin: '0px',
       threshold: 1.0,
       root: null,
